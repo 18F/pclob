84e6c48e77a58581e3921dc9e08a1a4c4af89365
Atul Varma <varmaa@gmail.com>
2017-08-01 07:29:15 -0400

    Some refactoring and better error handling.

--- _plugins/content_collection.rb
+++ _plugins/content_collection.rb
@@ -3,27 +3,37 @@ module Data_Model
     safe true
     priority :highest
 
+    def get_related_item(site, collection_name, slug)
+      collection = site.data[collection_name]
+      if not collection
+        raise "Unknown content-collection '#{collection_name}' for '#{slug}'"
+      end
+      related_item = nil
+      for candidate in collection
+        if candidate['slug'] == slug
+          related_item = candidate
+          break
+        end
+      end
+      if not related_item
+        raise ("Unable to find item in #{collection_name}.yml " +
+               "with slug '#{slug}'")
+      end
+      related_item
+    end
+
     def generate(site)
-      for report in site.data['oversight-reports']
+      source = 'oversight-reports'
+      for report in site.data[source]
         for item in report['related']
-          cc = item['content-collection']
-          slug = item['slug']
-          if cc
-            collection = site.data[cc]
-            if not collection
-              raise "Unknown content-collection for '#{slug}': '#{cc}'"
+          collection_name = item['content-collection']
+          if collection_name
+            begin
+              item['item'] = get_related_item(site, collection_name,
+                                              item['slug'])
+            rescue RuntimeError => e
+              raise "#{e} (encountered when processing #{source}.yml)"
             end
-            related_item = nil
-            for candidate in collection
-              if candidate['slug'] == slug
-                related_item = candidate
-                break
-              end
-            end
-            if not related_item
-              raise "Unable to find item in '#{cc}.yml' with slug '#{slug}'"
-            end
-            item['item'] = related_item
           end
         end
       end
