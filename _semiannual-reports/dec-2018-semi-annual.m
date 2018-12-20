8e48b7b3fdb48280c5be3822224d7a0b7ddffdb4
mluke22 <31956976+mluke22@users.noreply.github.com>
2018-12-20 11:23:23 -0500

    Create dec-2018-semi-annual.m

---
+++ _semiannual-reports/dec-2018-semi-annual.m
@@ -0,0 +1,11 @@
+---
+layout: report
+date: December 2018
+permalink: /reports/dec-2018-semi-annual/
+summary: >-
+   This report covers the period from October 2016-December 2018. From January 2017 until October 2018, the Board had too few Senate-confirmed Members to constitute a quorum, leaving it unable to issue Semi-Annual reports. Having regained its quorum, the Board is pleased to renew the practice of issuing Semi-Annual Reports.
+attachments:
+    - title: Open Report
+      url: /library/Semi-Annual Report - PCLOB Dec 2018.pdf
+title: Semi-Annual Report December 2018
+---
